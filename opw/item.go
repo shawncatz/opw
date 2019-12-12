676a2b068eb0a75e6d78647f80ca1252ad319ac4
Shawn Catanzarite <scatanzarite@sharespost.com>
2019-12-11 16:28:43 -0800

    initial

---
+++ opw/item.go
@@ -0,0 +1,59 @@
+package opw
+
+type Item struct {
+	UUID string
+	TemplateUUID string
+	Trashed string
+	CreatedAt string
+	UpdatedAt string
+	ChangerUUID string
+	ItemVersion int
+	VaultUUID string
+	Details struct {
+		Fields []struct {
+			Designation string
+			ID string
+			Name string
+			Type string
+			Value string
+		}
+		HTMLForm struct {
+			HTMLMethod string
+		}
+		Password string
+		PasswordHistory []struct {
+			Time int
+			Value string
+		}
+		Sections []struct {
+			Name string
+			Title string
+		}
+	}
+	Overview struct {
+		URLs []struct {
+			L string
+			U string
+		}
+		Ainfo string
+		PBE float64
+		Pgrng bool
+		Ps int
+		Tags []string
+		Title string
+		URL string
+	}
+}
+
+func (i *Item) Username() string {
+	return i.Overview.Ainfo
+}
+
+func (i *Item) Password() string {
+	for _, f := range i.Details.Fields {
+		if f.Type == "P" {
+			return f.Value
+		}
+	}
+	return ""
+}
---
+++ opw/opw.go
@@ -0,0 +1,21 @@
+package opw
+
+import (
+	"encoding/json"
+	"os/exec"
+)
+
+func GetItem(uuid string) (*Item, error){
+	out, err := exec.Command("/usr/local/bin/op", "get", "item", uuid).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	item := &Item{}
+	err = json.Unmarshal([]byte(out), item)
+	if err != nil {
+		return nil, err
+	}
+
+	return item, nil
+}
